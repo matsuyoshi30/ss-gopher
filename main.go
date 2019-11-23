9bdd5efcb58d1613fc7b4cdc4f94a075510b3b57
matsuyoshi <sfbgwm30@gmail.com>
2019-11-24 01:36:51 +0900

    fix

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"time"
 
+	"github.com/gdamore/tcell"
 	"github.com/rivo/tview"
 )
 
@@ -11,7 +12,41 @@ const refreshInterval = 500 * time.Millisecond
 var (
 	view *tview.Box
 	app  *tview.Application
+
+	flag = true
 )
+
+type Pic []string
+
+func (p Pic) width() int {
+	return len(p[0])
+}
+
+func (p Pic) height() int {
+	return len(p)
+}
+
+type Picmap map[int]Pic
+
+func draw(screen tcell.Screen, x int, y int, width int, height int) (int, int, int, int) {
+	var gopher Pic
+
+	if flag {
+		gopher = frontpage[1]
+	} else {
+		gopher = frontpage[2]
+	}
+
+	startY := (height - gopher.height()) / 2
+	for _, gopherStr := range gopher {
+		tview.Print(screen, gopherStr, x, startY, width, tview.AlignCenter, tcell.ColorWhite)
+		startY++
+	}
+
+	flag = !flag
+
+	return 0, 0, 0, 0
+}
 
 func run() error {
 	app = tview.NewApplication()
